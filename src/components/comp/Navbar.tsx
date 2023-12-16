7229b4c0ddeba78ba166bae2054f80407e0e6f63
GeroWalther <gero.walther@gmail.com>
2023-12-16 21:59:41 +0100

    finished auth2

--- src/components/comp/Navbar.tsx
+++ src/components/comp/Navbar.tsx
@@ -2,32 +2,36 @@ import Link from 'next/link';
 import { PlutoLogo } from '../svgs/Icons';
 import { buttonVariants } from '../ui/button';
 import Cart from './Cart';
+import { getServerSideUser } from '@/lib/payload-utils';
+import { cookies } from 'next/headers';
 
 import MaxWidthWrapper from './MaxWidthWrapper';
 import NavItems from './NavItems';
 
-export default function Navbar() {
-  const user = null;
+export default async function Navbar() {
+  const nextCookies = cookies();
+  const { user } = await getServerSideUser(nextCookies);
 
   return (
     <div className='bg-white sticky z-50 top-0 inset-x-0 h-16'>
       <header className='relative bg-white'>
         <MaxWidthWrapper>
-          <div className='border-b border-stone-200'>
+          <div className='border-b border-stone-900'>
             <div className='flex h-16 items-center'>
               {/* TODO: Mobile nav */}
 
               <div className=' ml-2 flex lg:ml-1'>
                 <Link href={'/'}>
-                  <PlutoLogo className='text-stone-900' />
+                  <PlutoLogo className='text-stone-200' />
                 </Link>
               </div>
               <div className='hidden z-50 lg:ml-6 lg:block lg:self-stretch'>
                 <NavItems />
               </div>
+
               <div className='ml-auto flex items-center'>
-                <div className=' hidden lg:flex lg:items-center lg:justify-end lg:space-x-6'>
-                  {user ?? (
+                <div className='hidden lg:flex lg:flex-1 lg:items-center lg:justify-end lg:space-x-6'>
+                  {user ? null : (
                     <Link
                       href='/sign-in'
                       className={buttonVariants({
@@ -36,30 +40,35 @@ export default function Navbar() {
                       Sign in
                     </Link>
                   )}
-                  {user ?? (
+
+                  {user ? null : (
                     <span
                       className='h-6 w-px bg-stone-200'
                       aria-hidden='true'
                     />
                   )}
+
                   {user ? (
-                    <p></p>
+                    <p>user</p>
                   ) : (
                     <Link
                       href='/sign-up'
-                      className={buttonVariants({ variant: 'ghost' })}>
+                      className={buttonVariants({
+                        variant: 'ghost',
+                      })}>
                       Create account
                     </Link>
                   )}
-                  {user && (
+
+                  {user ? (
                     <span
                       className='h-6 w-px bg-stone-200'
                       aria-hidden='true'
                     />
-                  )}
-                  {user ?? (
+                  ) : null}
+
+                  {user ? null : (
                     <div className='flex lg:ml-6'>
-                      {' '}
                       <span
                         className='h-6 w-px bg-stone-200'
                         aria-hidden='true'
@@ -67,7 +76,7 @@ export default function Navbar() {
                     </div>
                   )}
 
-                  <div className=' ml-4 flow-root lg:ml-6'>
+                  <div className='ml-4 flow-root lg:ml-6'>
                     <Cart />
                   </div>
                 </div>

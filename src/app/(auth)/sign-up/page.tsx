18b09d4d8b27f801b5413d3dbdaf86eb96883661
GeroWalther <gero.walther@gmail.com>
2023-12-16 18:22:14 +0100

    verify email5

--- src/app/(auth)/sign-up/page.tsx
+++ src/app/(auth)/sign-up/page.tsx
@@ -8,12 +8,15 @@ import { cn } from '@/lib/utils';
 import Link from 'next/link';
 import { useForm } from 'react-hook-form';
 import { zodResolver } from '@hookform/resolvers/zod';
+import { ZodError } from 'zod'
+import { useRouter } from 'next/navigation';
 
 import {
   AuthCredentialsValidator,
   TAuthCredentialsValidator,
 } from '@/lib/validators/account-credentials-validator';
 import { trpc } from '@/trpc/client';
+import { toast } from 'sonner';
 
 const Page = () => {
   const {
@@ -23,8 +26,26 @@ const Page = () => {
   } = useForm<TAuthCredentialsValidator>({
     resolver: zodResolver(AuthCredentialsValidator),
   });
+  const router = useRouter();
+  const { mutate, isLoading } = trpc.auth.createPayloadUser.useMutation({
+    onError: (err) => {
+      //@ts-ignore
+      if (err.data?.code === 'CONFLICT') {
+        toast.error('This email is already in use. Sign in instead?');
+        return;
+      }
 
-  const { mutate, isLoading } = trpc.auth.createPayloadUser.useMutation({});
+      if (err instanceof ZodError) {
+        toast.error(err.issues[0].message);
+        return;
+      }
+      toast.error('Something went wrong. Please try again.');
+    },
+    onSuccess: ({ sentToEmail }) => {
+      toast.success(`Verification email sent to %{sentToEmail}.`);
+      router.push('/verify-email?to=' + sentToEmail);
+    },
+  });
 
   const onSubmit = ({ email, password }: TAuthCredentialsValidator) => {
     mutate({ email, password });
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import Providers from '@/components/comp/Providers';
 import { cn } from '@/lib/utils';
 import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
+import { Toaster } from 'sonner';
 import './globals.css';
 
 const inter = Inter({ subsets: ['latin'] });
@@ -30,6 +31,8 @@ export default function RootLayout({
             <div className='flex-grow flex-1'>{children}</div>
           </Providers>
         </main>
+
+        <Toaster position='top-center' richColors />
       </body>
     </html>
   );

8b60fda48b9c05435395451c5144a0d3840ee5d4
GeroWalther <gero.walther@gmail.com>
2023-12-30 18:07:51 +0100

    ts-ignore added 2

--- next.config.js
+++ next.config.js
@@ -10,11 +10,11 @@ const nextConfig = {
       },
     ],
   },
-  typescript: {
-    // Warning: This allows production builds to successfully complete even if
-    // your project has ESLint errors.
-    ignoreBuildErrors: true,
-  },
+  // typescript: {
+  //   // Warning: This allows production builds to successfully complete even if
+  //   // your project has ESLint errors.
+  //   ignoreBuildErrors: true,
+  // },
 };
 
 module.exports = nextConfig;

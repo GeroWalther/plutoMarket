a64c3071ab12b79cd67df7761b2b22408a6855da
GeroWalther <gero.walther@gmail.com>
2024-01-09 19:47:45 +0100

    cart logic updated

--- src/hooks/use-cart.ts
+++ src/hooks/use-cart.ts
@@ -23,22 +23,41 @@ export const useCart = create<CartState>()(
       items: [],
       addItem: (product) =>
         set((state) => {
+          // this logic prevents adding items with the same id
+          const existingItem = state.items.find(
+            (item) => item.product.id === product.id
+          );
+          if (existingItem) {
+            return state;
+          }
           return { items: [...state.items, { product }] };
+
+          // if we want to allow for multiple items with the same id in the cart use this code:
+          // set((state) => {
+          //   return { items: [...state.items, { product }] };
+          // }),
         }),
       removeItem: (id) =>
         set((state) => {
-          const itemIndex = state.items.findIndex(
-            (item) => item.product.id === id
+          // if we can have multiple items with the same id this logic removes the one of them
+          //   const itemIndex = state.items.findIndex(
+          //     (item) => item.product.id === id
+          //   );
+
+          //   if (itemIndex !== -1) {
+          //     const updatedItems = [...state.items];
+          //     updatedItems.splice(itemIndex, 1);
+
+          //     return { items: updatedItems };
+          //   }
+
+          //   return state;
+          // in addItem we have a logic that prevents multiple items of the same id to be added so we go with below logic
+          const updatedItems = state.items.filter(
+            (item) => item.product.id !== id
           );
 
-          if (itemIndex !== -1) {
-            const updatedItems = [...state.items];
-            updatedItems.splice(itemIndex, 1);
-
-            return { items: updatedItems };
-          }
-
-          return state;
+          return { items: updatedItems };
         }),
       clearCart: () => set({ items: [] }),
     }),

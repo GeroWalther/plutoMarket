78185f3edfc828d93b8b5a28a11b391e5fcbdce7
GeroWalther <gero.walther@gmail.com>
2024-02-20 12:25:49 +0100

    -

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,5 @@
 import express from 'express';
-import { getPayloadClient } from './get-payload';
+import { getPayloadClient } from './get-payload.ts';
 import { nextApp, nextHandler } from './next-utils';
 import * as trpcExpress from '@trpc/server/adapters/express';
 import { appRouter } from './trpc';
